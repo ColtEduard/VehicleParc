a1ed32fd975322b2a23a88fe1f59ad252ab8216a
CerealBread <45251074+CerealBread@users.noreply.github.com>
2019-01-17 10:06:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 
+
 #include "Vehicle.h"
 #include "Car.h"
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <fstream>
+#include <vector>
+#ifndef WINPAUSE
+#define WINPAUSE system("pause");
+#endif
+
+
+
+vector<string> split(const string& str, const string& delim)
+{
+	vector<string> tokens;
+	size_t prev = 0, pos = 0;
+	do
+	{
+		pos = str.find(delim, prev);
+		if (pos == string::npos) pos = str.length();
+		string token = str.substr(prev, pos - prev);
+		if (!token.empty()) tokens.push_back(token);
+		prev = pos + delim.length();
+	} while (pos < str.length() && prev < str.length());
+	return tokens;
+}
+
 
 int main()
 {
@@ -24,15 +47,44 @@ int main()
 		getline(input, token, ", ");
 	}
 	*/
+	vector<Vehicle> vehicles;
+	
 
+	string line;
+	ifstream myfile("vehicles.txt");
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			vector<string> v = split(line, ",");
+			//vehicles.push_back(Vehicle(stoi(v[0]), v[1], stod())
+			for (string const& value : v) {
+				std::cout << value << " ";
+			}
+			cout << endl;
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+
+	/*ofstream myfile;
+	myfile.open("example.txt");
+	myfile << "Writing this to a file.\n";
+	myfile.close();*/
+
+	//Vehicle(numberWheels, colour, fuel, brand, cylindricalCapacity, numberSeats, year, consumption)
+	//Car(colour, fuel, brand, cylindricalCapacity, numberSeats, year, consumption)
+	
 	Vehicle vehicle(4, "red", "gas", "VW", 2000, 2, 2001, 4.5);
 	vehicle.display();
 
-	Car car(2, "bleu", "petrol", "Golan", 1200, 2, 2021, 6.7);
+	Car car("bleu", "petrol", "Golan", 1200, 2, 2021, 6.7);
 	car.display();
 
+	Car car2("black", "gas", "Golan", 3000, 1, 2020, 3.5);
+	car2.display();
 
-
-
+	WINPAUSE
+}
 }
 
