5be83b2f36d1de618610f05c3dd1f9f8f5d89cd1
CerealBread <45251074+CerealBread@users.noreply.github.com>
2019-01-17 15:54:06 +0200

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -2,16 +2,19 @@
 #define VEHICLE_H
 
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 
 
 
-class Vehicle {
+class Vehicle{
+
+
 
 protected:
-	static int globalID;
-	int id;
+    
+    static int globalID;
+    int id;
 	int numberWheels;
 	string colour;
 	string fuel;
@@ -20,56 +23,51 @@ protected:
 	int numberSeats;
 	int year;
 	double consumption;
-	string type;
+    string type;
 
-public:
+public: 
 
-	Vehicle();
+    Vehicle();
 	Vehicle(int, string, string, string, int, int, int, double);
 
 	~Vehicle();
 	/*
-	// Fuel type
-	const int GAS, DIELES, HYDROGEN;
+    // Fuel type
+    const int GAS, DIELES, HYDROGEN;
 
-	// colours
-	const int RED, GREEN, BLUE, WHIE, BLACK, YELLOW, BROWN, ORANGE;
+    // colours
+    const int RED, GREEN, BLUE, WHIE, BLACK, YELLOW, BROWN, ORANGE;
 
-	// brands
-	const int MERCEDES, VOLKSWAGEN, GMC, BMW;
+    // brands
+    const int MERCEDES, VOLKSWAGEN, GMC, BMW;
 	*/
 
 	// SETTERS
 	void setId(int);
-	void setNumberWheels(int);
-	void setCoulour(string);
-	void setFuel(string);
-	void setBrand(string);
-	void setCylindricalCapacity(int);
-	void setNumberSeats(int);
-	void setYear(int);
-	void setConsumption(double);
+    void setNumberWheels(int);
+    void setCoulour(string);
+    void setFuel(string);
+    void setBrand(string);
+    void setCylindricalCapacity(int);
+    void setNumberSeats(int);
+    void setYear(int);
+    void setConsumption(double);
 
 	// GETTERS
 	int getId();
-	int getNumberWheels();
-	string getCoulour();
-	string getFuel();
-	string getBrand();
-	int getCylindricalCapacity();
-	int getNumberSeats();
-	int getYear();
-	double getConsumption();
-	string getType();
+    int getNumberWheels( );
+    string getCoulour();
+    string getFuel();
+    string getBrand();
+    int getCylindricalCapacity();
+    int getNumberSeats();
+    int getYear();
+    double getConsumption();
+    string getType();
 
 	// UTILITY
 	void display();
-
-
-
-
-
-
+   
 
 };
 
